583199e4c3c1a2ae4aa35e9f863074cd4913cb15
Felipex945 <felipe.legal940@gmail.com>
2026-09-16 15:02:28 -0300

    Add files via upload

---
+++ Python/Curso prof.Guanabara/Python Aula 14/Aula14ex60.py
@@ -0,0 +1,5 @@
+'''from math import factorial
+numero = int(input("Digite um valor para calcular seu fatorial: "))
+fatorial = factorial(numero)
+print(f"O fatorial do número {numero} é {fatorial}")'''
+
